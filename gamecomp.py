0cbf93ae5a15b6173eb35ce0d3797e4dfa8694da
Taisuke Maekawa <fygar256@outlook.jp>
2025-12-08 05:53:03 +0900

    Update gamecomp.py

--- gamecomp.py
+++ gamecomp.py
@@ -380,7 +380,7 @@ def main():
 
 if __name__=='__main__':
     if len(sys.argv)!=2:
-        print("Usage: miep2.py file.gm >out.c")
+        print("Usage: gamecomp.py file.gm >out.c")
         exit(1)
     main()
     exit(0)
